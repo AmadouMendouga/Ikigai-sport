0454718755e0146453caaa22a16dd115b0e82270
AmadouMendouga <amadou69mendouga@gmail.com>
2026-09-13 18:43:02 +0100

    Load final V2 accessibility fixes

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -7,6 +7,7 @@ import "./ikigai-refonte-v2-components.css";
 import "./ikigai-rareui-polish.css";
 import "./ikigai-commerce-polish.css";
 import "./ikigai-journey-polish.css";
+import "./ikigai-refonte-v2-finalize.css";
 import { IconSprite } from "@/components/icons/IconSprite";
 import { ToastHost } from "@/components/Toast";
 import { Analytics } from "@vercel/analytics/next";
