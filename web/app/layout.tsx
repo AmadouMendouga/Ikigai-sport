5c204afc3257037b21da36e11ba0c1fd99dd0509
AmadouMendouga <amadou69mendouga@gmail.com>
2026-09-13 18:24:45 +0100

    Load commerce detail and cart polish

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -5,6 +5,7 @@ import "./ikigai-ux-polish.css";
 import "./ikigai-refonte-v2.css";
 import "./ikigai-refonte-v2-components.css";
 import "./ikigai-rareui-polish.css";
+import "./ikigai-commerce-polish.css";
 import { IconSprite } from "@/components/icons/IconSprite";
 import { ToastHost } from "@/components/Toast";
 import { Analytics } from "@vercel/analytics/next";
